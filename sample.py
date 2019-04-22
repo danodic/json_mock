2ad054302bcec689ebd50783c2375618bcdbbc53
Danilo Guimaraes <danilo.guimaraes@e-core.com>
2019-04-22 18:08:27 -0300

    Adding sample test.

---
+++ sample.py
@@ -0,0 +1,18 @@
+import requests
+
+# Do the request and get the response
+response = requests.get('https://my-json-server.typicode.com/danodic/json_mock/request_track_list')
+
+# Assert the response code
+assert(response.status_code)
+
+# Convert response to a dictionary
+data = response.json()
+
+# Do the asserts
+assert(data['artist'] == 'The Beatles')
+assert(len(data['tracklist']) == 3)
+
+# Assert one of the tracks
+assert(data['tracklist'][0]['name'] == 'Yellow Submarine')
+assert(data['tracklist'][0]['length'] == '2:39')
